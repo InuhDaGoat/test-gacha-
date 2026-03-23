357200265af658b6ab53429fa04b22fa5da40ae2
InuhDaGoat <inuhwabackup@gmail.com>
2026-03-23 17:29:06 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,3 +1,4 @@
+// supabase.js
 const SUPABASE_URL = "https://galsberhhslhbnesagcc.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhbHNiZXJoaHNsaGJuZXNhZ2NjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNDg5MTMsImV4cCI6MjA4OTgyNDkxM30.IZzm9ukiyolJm0uUlZfGIqkRpbQvetGhCNI-bP7dtJk";
 
